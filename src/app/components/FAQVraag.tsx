152514b333b1c56ec9155ba3aa19cf92134dbd99
DeanStavenuiter <ddstavenuiter@gmail.com>
2024-03-20 14:20:36 +0100

    FAQ page

---
+++ src/app/components/FAQVraag.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+
+interface FAQVraagProps {
+  vraag: string;
+  antwoord: string;
+}
+
+const FAQVraag = ({ vraag, antwoord }: FAQVraagProps) => {
+  const [open, setOpen] = useState<Boolean>(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div
+      className=' flex flex-col w-[554px] cursor-pointer rounded-[15px] border border-[#1C355C4D] bg-[#1A305508] p-[30px_50px_34px_45px]'
+      onClick={handleClick}
+    >
+      <div className='flex-row items-center justify-between flex'>
+        <span className='text-[16px] font-normal tracking-tight'>{vraag}</span>
+        <span>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            height='24'
+            viewBox='0 -960 960 960'
+            width='24'
+            className={`transform cursor-pointer ${!open ? 'rotate-180-Close' : 'rotate-180-Open'}`}
+          >
+            <path d='M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z' />
+          </svg>
+        </span>
+      </div>
+
+      <div className={`${open ? '' : 'hidden'} text-[16px] font-normal tracking-tight mt-[10px] w-[435px]`}>{antwoord}</div>
+    </div>
+  );
+};
+
+export default FAQVraag;
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import FooterDesktop from '../components/FooterDesktop';
+import { FooterMobile } from '../components/FooterMobile';
+import BlauweBalk from '../components/BlauweBalk';
+import BelOns from '../components/BelComponent';
+import FAQVraag from '../components/FAQVraag';
 
 const faq = () => {
   return (
-    <div>faq</div>
-  )
-}
+    <div className='max-h-[-webkit-fill-available]'>
+      <Navbar />
 
-export default faq
+      <div>
+        <div className='mb-[41px] mt-[76px] flex flex-col items-center justify-center'>
+          <h1 className='text-[69px] font-black uppercase tracking-tight text-[#1C355D] opacity-[0.07]'>
+            faq
+          </h1>
+          <h2 className='text-[45px] font-semibold tracking-tight text-[#1C355D] '>
+            Veel gestelde vragen
+          </h2>
+        </div>
+        <div className='mb-[64px] flex flex-col items-center justify-center'>
+          <div className='mb-[25px] w-[360px]'>
+            <p className='text-[15px]/[25px] font-normal tracking-tight text-[#182228]'>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut
+            </p>
+          </div>
+          <div className='btn rounded-[5px] hover:border-[#E98A14] border-[#E98A14] hover:bg-white bg-white p-[12px_47px_14px_48px] '>
+            <span className='text-[16px] font-medium tracking-tighter text-[#E98A14]'>
+              Neem contact met ons op
+            </span>
+          </div>
+        </div>
+        <div className='flex justify-center'>
+          <div className='flex'>
+            <div className='flex w-[1200px] flex-row text-[20px] font-medium text-[#1C355C]'>
+              <div className='w-[50%]'>
+                <h6 className='mb-[41px]'>Onderwerp 1</h6>
+                <div className='flex flex-col gap-[15px]'>
+                  <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                </div>
+              </div>
+              <div className='w-[50%] mb-[229px]'>
+                <h6 className='mb-[41px]'>Onderwerp 2</h6>
+                <div className='flex flex-col gap-[15px]'>
+                  <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                <FAQVraag
+                  vraag='Hier komt een veelgestelde vraag te staan'
+                  antwoord='  Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat Lorem ipsum dolor sit amet, consetetur'
+                />
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <BelOns />
+      <BlauweBalk />
+      <FooterDesktop />
+      <FooterMobile />
+    </div>
+  );
+};
+
+export default faq;
